e2737c2c30620c814251d0b9ccb3c2d5e327508c
xpimenta <matheuspr96@hotmail.com>
2025-10-08 15:47:24 +0100

    img content

---
+++ src/app/compartidos/componentes/input-img/input-img.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-img',
+  imports: [],
+  templateUrl: './input-img.component.html',
+  styleUrl: './input-img.component.css'
+})
+export class InputImgComponent {
+
+}
